d5c01810da9243d7a76708e9912369b028580eed
Abhijay (Jay) Nair <abhijaynair9@gmail.com>
2024-07-09 16:30:28 -0400

    Update AboutSection.tsx
    Updated about section to indicate graduation status

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -11,11 +11,7 @@ const AboutSection = () => {
             </h2>
           </div>
           <div className="text-left col-span-8 text-white lg:text-xl pt-6">
-            A Master&apos;s in Computer Science student on track to graduate in
-            May 2024. Focussing in Artificial Intelligence, I have a holistic
-            experience in Computer Vision applications involving pose
-            estimation, image analysis and classification using the frequency
-            spectrum.
+            I hold a Master&apos;s degree in Computer Science with a specialization in Artificial Intelligence, having graduated in May 2024. My studies encompassed a broad range of Computer Vision applications, particularly in depth and pose estimation, speed approximation of small and fast-moving objects, and classification utilizing the frequency spectrum.
             <br />
             While I continue to honor my skills in deep learning, I&apos;ve
             always had a profound interest in developing frontend applications,
