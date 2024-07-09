0d50168bf4d048467f3c2caeec55dc3af1c6dfa2
Abhijay (Jay) Nair <abhijaynair9@gmail.com>
2024-07-09 16:31:04 -0400

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-10 flex justify-between">
-        <span>&copy;Abhijay Nair, 2023</span>
+        <span>&copy;Abhijay Nair, 2024</span>
         <p className="text-slate-600 ml-3">&#9749; Brewed with 	&#10084; using <span className="font-semibold">Next.js</span> and <span className="font-semibold">tailwindcss</span></p>
       </div>
     </footer>
