77965b1d86d18dc7f4c41554874648db2595e420
Jay Nair <abhijaynair@icloud.com>
2024-09-22 21:31:57 -0400

    updated experience

--- app/components/ExperienceSection.tsx
+++ app/components/ExperienceSection.tsx
@@ -5,6 +5,7 @@ import { vbg } from "./experience_bullets/vbg";
 import { rit } from "./experience_bullets/rit";
 import { tcs } from "./experience_bullets/tcs";
 import { uMum } from "./experience_bullets/uMum";
+import { ingenid_ft } from "./experience_bullets/ingenid_ft";
 
 const ExperienceSection = () => {
   const [uMumVisible, setUMumVisible] = useState(false);
@@ -12,7 +13,7 @@ const ExperienceSection = () => {
   const [tcsVisible, setTcsVisible] = useState(false);
   const [vbgVisible, setVbgVisible] = useState(false);
   const [ingenIdVisible, setIngenIdVisible] = useState(false);
-
+  const [ingenIdFtVisible, setIngenIdFtVisible] = useState(false)
   return (
     <section className="lg:py-20 py-10" id="experience">
       <div>
@@ -21,6 +22,7 @@ const ExperienceSection = () => {
         </h2>
         <div className="py-4">
           <ul className="flex flex-col space-y-5">
+            {ingenid(setIngenIdFtVisible, ingenIdFtVisible)}
             {ingenid(setIngenIdVisible, ingenIdVisible)}
             {vbg(setVbgVisible, vbgVisible)}
             {tcs(setTcsVisible, tcsVisible)}
---
+++ app/components/experience_bullets/ingenid_ft.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React from "react";
+
+export function ingenid_ft(
+  setIngenIdFtVisible: React.Dispatch<React.SetStateAction<boolean>>,
+  ingenIdFtVisible: boolean
+) {
+  return (
+    <li
+      className="border border-[#33353F] rounded-lg w-full px-4 py-4 bg-[#18191E]"
+      onClick={() => setIngenIdFtVisible(!ingenIdFtVisible)}
+    >
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-col">
+          <h4 className="col-span-4 px-2 text-2xl font-semibold text-middle">
+            IngenID LLC
+          </h4>
+          <h6 className="col-span-4 px-2 text-gray-400">
+            Software Engineer
+          </h6>
+          {ingenIdFtVisible && (
+            <div className="flex flex-col px-2 py-1">
+              <div className="flex flex-row justify-between rounded-md w-fit">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-map-pin"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="#8fa3b8"
+                  fill="none"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                  <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                </svg>
+                <div className="ml-2 text-slate-400">Rochester, New York</div>
+              </div>
+              <div className="px-2 py-1 text-slate-300">
+                <ul className="list-disc px-4">
+                  <li>
+                    Leveraged Google Cloud and Firebase Cloud Messaging voice
+                    authentication requests to a target user device.
+                  </li>
+                  <li>
+                    Achieved a significant reduction in prototyping sprint for Deep
+                    Learning models by utilizing Python and Docker to streamline the CI/CD process.
+                  </li>
+                </ul>
+              </div>
+              <ul className="flex flex-row flex-wrap justify-center w-fit space-x-2">
+                <li className="text-slate-400 col-span-4 border border-slate-500 rounded-3xl w-fit mt-2 hover:bg-secondary hover:text-white">
+                  <div className="py-2 px-6 text-center font-medium">Flutter</div>
+                </li>
+                <li className="text-slate-400 col-span-4 border border-slate-500 rounded-3xl w-fit mt-2 hover:bg-middle hover:text-black">
+                  <div className="py-2 px-6 text-center font-medium">React</div>
+                </li>
+                <li className="text-slate-400 col-span-4 border border-slate-500 rounded-3xl w-fit mt-2 font-medium hover:bg-primary hover:text-black">
+                  <div className="py-2 px-6 text-center font-medium">Docker</div>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col justify-center">
+          <div className="flex flex-row">
+            <h6 className="col-span-4 invisible lg:visible px-2 text-gray-300">
+              2024
+            </h6>
+            <div
+              className={`transition-all transform ${
+                ingenIdFtVisible ? "rotate-180" : "rotate-0"
+              }`}
+            >
+              <svg
+                className="icon icon-tabler icon-tabler-chevron-down"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M6 9l6 6l6 -6" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
