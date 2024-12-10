7f52c3dcbf7ed04cf3af3b518d986075281962d7
Jay Nair <abhijaynair@icloud.com>
2024-12-09 21:06:50 -0500

    removed contact section

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <ExperienceSection />
           <SkillsSection />
           <ProjectsSection />
-          <ContactSection />
+          {/* <ContactSection /> */}
         </div>
       </main>
       <Footer />
