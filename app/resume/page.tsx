6b16aac53f9b357f7905423d0ba14c75f1bc8fcb
Jay Nair <abhijaynair@icloud.com>
2024-12-14 19:57:16 -0500

    new path

---
+++ app/resume/page.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/Navbar"
+import Footer from "../components/Footer"
+
+export default function Resume() {
+    <div className="mb-0 bg-[#141414] scroll-smooth">
+    <main className="flex min-h-screen flex-col bg-[#141414]">
+      <Navbar />
+    </main>
+    <Footer />
+  </div>
+}
