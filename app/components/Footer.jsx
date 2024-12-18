7484d2d1571b51154ac9d6d2cc7b259b3ab83e9a
Jay Nair <abhijaynair@icloud.com>
2024-12-18 11:51:05 -0500

    fixed footer border

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
       <div className="container p-10 flex justify-between">
         <span>&copy;Abhijay Nair, 2024</span>
         <p className="text-slate-600 ml-3">&#9749; Brewed with 	&#10084; using <span className="font-semibold">Next.js</span> and <span className="font-semibold">tailwindcss</span></p>
